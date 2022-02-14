5efc23ee1b537c0db076f1a240933c68ca89b62f
nour-alsatari <nouress98@gmail.com>
2022-02-14 23:35:04 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-// import logo from './logo.svg';
+import { Routes, Route, } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/Home";
+import TourDetails from './components/tourDetails/TourDetails.js'
 
 const travelData = require("./data/db.json");
 
 function App() {
   return (
     <>
-      <Home jsonData={travelData} />
+      <Routes>
+        <Route path="/" element={<Home jsonData={travelData} />} />
+        <Route path="/city/:id" element={<TourDetails jsonData={travelData} />} />
+      </Routes>
     </>
   );
 }
