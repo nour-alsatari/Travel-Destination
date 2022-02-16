ec4571d71550d050d2f9bc4519836831494518c8
nour-alsatari <nouress98@gmail.com>
2022-02-16 12:45:17 +0200

    done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,9 @@
 import headerStyle from "./Header.css";
-import { Link } from "react-router-dom";
+import Nav from "../navbar/Navbar";
 
 function Header() {
   return (
     <>
-      <nav>
-        <Link to="/"> Home </Link>
-      </nav>
       <h1> Here is a list of the most visited places around the world</h1>
     </>
   );
